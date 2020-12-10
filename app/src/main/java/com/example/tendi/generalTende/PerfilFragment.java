9737f19bcf0bc10ca18b43915d107d6e48546102
jhonrs <jhonrs812@gmail.com>
2020-12-10 15:09:41 -0500

    final profile and styles buttons

--- app/src/main/java/com/example/tendi/generalTende/PerfilFragment.java
+++ app/src/main/java/com/example/tendi/generalTende/PerfilFragment.java
@@ -1,12 +1,17 @@
 package com.example.tendi.generalTende;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.tendi.R;
 
@@ -14,6 +19,10 @@ import com.example.tendi.R;
 public class PerfilFragment extends Fragment {
 
 
+    private TextView propietariotv;
+    private TextView dinerotv;
+    private String text;
+    private String dinero;
 
     public PerfilFragment() {
         // Required empty public constructor
@@ -32,6 +41,31 @@ public class PerfilFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil, container, false);
+
+        View root = inflater.inflate(R.layout.fragment_perfil, container, false);
+
+        propietariotv = root.findViewById(R.id.textView56);
+        dinerotv = root.findViewById(R.id.textView60);
+
+
+        text = "Propietario: Oscar Giraldo";
+        SpannableStringBuilder ss = new SpannableStringBuilder(text);
+        ForegroundColorSpan fscBlack = new ForegroundColorSpan(Color.DKGRAY);
+        ss.setSpan(fscBlack,13, 26, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        propietariotv.setText(ss);
+
+
+        dinero = "$ 1’547.500";
+        SpannableStringBuilder ss2 = new SpannableStringBuilder(dinero);
+        ForegroundColorSpan fscGreen = new ForegroundColorSpan(Color.parseColor("#38BF7E"));
+        ss2.setSpan(fscGreen,0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        dinerotv.setText(ss2);
+
+
+        return root;
+
+
     }
 }
